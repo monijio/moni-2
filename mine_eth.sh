b127c42adb5649bd67ab7c944da121d50a97a894
monijio <78681827+monijio@users.noreply.github.com>
2021-02-09 17:29:17 -0800

    don

---
+++ mine_eth.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./miner --algo ethash --server ethash.unmineable.com:3333 --user TSFBJTia257spnyNbM4EePmtkX76XAVQNC.1
---
+++ mine_monerov.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./miner --algo cuckarood29v --server xmrpool.eu:5555 --user 831s6SD4SVkTpt9w9iPe3eT1uc9nwropmapB6QAZVrMTSxSJicnTtNbJ7EzY6VprtGXwq6cG9497rdqRQhfqQBiWPbeFzCu.1
